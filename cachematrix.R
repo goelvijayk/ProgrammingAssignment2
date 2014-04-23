9a73153a3e2bd843b33418d2e3ba49de34118b36
goelvijayk <vijay.goelk@gmail.com>
2014-04-23 02:15:51 -0700

    testing commit
    testing a commit. function works but not completed with comments etc.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  c<- ncol(x)
+  r<- nrow(x)
+  #m<<- x
+  inv<<- matrix(NA, ncol=r, nrow=c)
+  return(inv)
 }
 
 
@@ -12,4 +16,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  if(!exists("inv", mode = "matrix")) {makeCacheMatrix(m)}
+  if(!exists("x_computed")) {x_computed <<- matrix(NA, nrow=nrow(x), ncol=ncol(x))}  
+    if (identical(x,x_computed) & sum(!is.na(inv)) ==0) {}
+    else 
+      {inv<<-  solve(x)
+      x_computed <<- x} 
+
+  return(inv)
+
 }
