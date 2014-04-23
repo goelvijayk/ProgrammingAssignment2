829c4f3f5d2686af32926ccd3b9d542902868f5c
goelvijayk <vijay.goelk@gmail.com>
2014-04-23 03:04:53 -0700

    Pre-final draft for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Get inverse of a square invertible matrix, preferably through cache if already computed.
+## assumes square matrix, and invertible matrix input
 
-## Write a short comment describing this function
-
+## Creates blank inverse matrix, as structure
 makeCacheMatrix <- function(x = matrix()) {
-  c<- ncol(x)
-  r<- nrow(x)
-  #m<<- x
-  inv<<- matrix(NA, ncol=r, nrow=c)
+  
+  #count of rows and columns to create square inverse matrix
+  r<- c<- ncol(x) 
+  
+  #create the matrix with all elements as NA
+  inv<<- matrix(NA, ncol=r, nrow=c) 
+  
   return(inv)
 }
 
 
-## Write a short comment describing this function
-
+## Returns a matrix inverse of x, preferably from cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   
-  if(!exists("inv", mode = "matrix")) {makeCacheMatrix(m)}
+  #check if structure of inverse matrix exists; else create;
+  if(!exists("inv")) {makeCacheMatrix(x)}
+  
+  #check if base matrix has been cached before to check if it changes after inverse computation
+  #create blank structure if doesn't exist
   if(!exists("x_computed")) {x_computed <<- matrix(NA, nrow=nrow(x), ncol=ncol(x))}  
-    if (identical(x,x_computed) & sum(!is.na(inv)) ==0) {}
-    else 
-      {inv<<-  solve(x)
-      x_computed <<- x} 
+  
+  #if earlier computed matrix is identical as new matrix, and inverse has been computed before(not NA), then do nothing
+  #else compute inverse of matrix and cache inverse matrix and input matrices
+  if (identical(x,x_computed) & sum(is.na(inv)) ==0) {}
+  else 
+    {
+    inv<<-  solve(x)
+    x_computed <<- x
+    }
 
+  #return the inverse matrix
   return(inv)
 
 }
